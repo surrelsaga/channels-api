128a71339ea559bdddaa1ec315406ff85949e0ee
Daniel Nguyen <233656241+DucAnh0106@users.noreply.github.com>
2026-08-04 16:05:52 +0800

    M2: add new channel object with name, server-generated id and timeCreated on POST request and return 201 created status

---
+++ src/data/channels.ts
@@ -0,0 +1,7 @@
+export interface Channel {
+    name: string,
+    id: string,
+    createdAt: string
+}
+
+export const channels: Channel[] = [];
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import type {Express, Request, Response} from 'express'
+import crypto from 'crypto';
+
+import { channels } from './data/channels.ts'
+import type { Channel } from './data/channels.ts';
 
 const app: Express = express();
 const port = 3000;
+
+// expression.json() middleware
+app.use(express.json());
 
 app.listen(port, (): void => {
     console.log(`Listening at port ${port}`);
@@ -10,4 +17,27 @@ app.listen(port, (): void => {
 
 app.get('/health', (req: Request, res: Response) => {
     res.send({ "status": "ok" });
+});
+
+app.get('/channels', (req: Request, res: Response) => {
+    // reply with an array of all the channels created
+    res.send(channels);
+});
+
+app.post('/channels', (req: Request, res: Response) => {
+    const id = crypto.randomUUID();
+    const timeCreated = new Date().toISOString();
+    const name = req.body.name;
+
+    const createdChannel: Channel = {
+        name: name,
+        id: id,
+        createdAt: timeCreated
+    };
+
+    // update to the "database" channels: a module
+    channels.push(createdChannel);
+
+    // return with 201 status code and the object
+    res.status(201).send(createdChannel);
 });
