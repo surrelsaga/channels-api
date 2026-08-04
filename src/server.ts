e6214e418571a2212b3f70e608c57b3550eb3649
Daniel Nguyen <233656241+DucAnh0106@users.noreply.github.com>
2026-08-04 10:40:28 +0800

    add GET /health returning 200 and a json containing status code

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
-// example code
-
 import express from 'express';
 import type {Express, Request, Response} from 'express'
 
 const app: Express = express();
-const port = 8000;
+const port = 3000;
 
 app.listen(port, (): void => {
     console.log(`Listening at port ${port}`);
 })
 
-app.get('/', (req: Request, res: Response) => {
-    res.json({});
+app.get('/health', (req: Request, res: Response) => {
+    res.send({ "status": "ok" });
 });
