09af57e210f4014d39ae83f8c5991321811a2fe7
Daniel Nguyen <233656241+DucAnh0106@users.noreply.github.com>
2026-08-07 13:52:53 +0800

    M3: validate channel existence and message body with 404/400 json errors

--- src/server.ts
+++ src/server.ts
@@ -41,3 +41,48 @@ app.post('/channels', (req: Request, res: Response) => {
     // return with 201 status code and the object
     res.status(201).send(createdChannel);
 });
+
+// hover to check the interface for Request object
+// Request<Params, ResBody, ReqBody, ReqQuery>
+const createMessage = (req: Request<{ channelId: string }, unknown, any>, res: Response) => {
+    // the channelId will be stored inside req.params
+    // this Id will be checked inside database, if don't exist then return 404
+    const channelId = req.params.channelId;
+    
+    // returns undefined if nothing is found, undefined is falsy
+    if( channels.find(channel => channel.id === channelId) === undefined ) {
+        res.status(404).send({ error: 'Channel not found' });
+
+        // response sent, stop here
+        return;
+    }
+
+    // the POST content will be stored inside req.body
+    const message = req.body;
+
+    // filter for the message
+    // ideal: { "body" : "message_here" }
+    // bad input:  "", "   ", undefined, null, 0
+    // except for "   ", everything else is falsy
+
+    // check if it's a string
+    if ( typeof message.body !== 'string' ) {
+        res.status(400).send({ error: 'message must be a string.' });
+
+        // response sent, stop here
+        return;
+    }
+    // it's a string already so just one more filter for case of "   "
+    else if (!message.body.trim()) {
+        res.status(400).send({ error: 'message body must be a non-empty string' });
+
+        // response sent, stop here
+        return;
+    }
+
+    // if everything is fine. 201 is 200 but with a resource (message) created
+    res.status(201).json({ 'messageSent': true });
+};
+
+// the colon is to specify that sth is a parameter in the (POST) request: req.params
+app.post('/channels/:channelId/messages', createMessage);
